582caae2028510bc9d2fdc3fd21de98fb3a0964b
Shorotshishir <tanzim121@gmail.com>
2018-12-25 21:56:28 +0600

    Updates

---
+++ precache-manifest.50c72bff2575dd1fe3fc537bcb2c13c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "748a1abf222c626312f2",
+    "url": "/static/js/main.748a1abf.chunk.js"
+  },
+  {
+    "revision": "80dd6a2ec0cb4c12403e",
+    "url": "/static/js/1.80dd6a2e.chunk.js"
+  },
+  {
+    "revision": "748a1abf222c626312f2",
+    "url": "/static/css/main.6a2282a0.chunk.css"
+  },
+  {
+    "revision": "01c91b25314629a9417b235749351c2d",
+    "url": "/index.html"
+  }
+];
